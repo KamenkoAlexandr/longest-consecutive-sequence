0d4a62bc4d3027056e67757b371012ab7cde42a2
Alexandr <34417026+KamenkoAlexandr@users.noreply.github.com>
2018-02-27 10:37:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,13 @@
 module.exports = function longestConsecutiveLength(array) {
-  //bad solution, first version  
-  function compareNumeric(a, b) {
-        if (a > b) return 1;
-        if (a < b) return -1;
-    }
-    function find(array, value) {
-        for (var i = 0; i < array.length; i++) {
-            if (array[i] === value) return i;
-        }
-        return -1;
-    }
-    var array2 = [];
-    for (var j = 0; j < array.length; j++) {
-        if (find(array2, array[j]) === -1) {
-            array2.push(array[j]);
-        }
-    }
-    array2.sort(compareNumeric);
-    var k = 1, max = 0;
-    for (var i = 0; i < array2.length; i++) {
-        if (array2[i] === array2[i+1]-1) k++;
+    let set = new Set(array);
+    let k = 1, max = 0;
+    for (let value of set) {
+        if (set.has(value + 1)) k++;
         else {
             max = Math.max(k, max);
             k = 1;
         }
+        max = Math.max(k, max)
     }
     return max;
 };
